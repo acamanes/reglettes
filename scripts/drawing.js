d9121ab1458db6ced9a9d7d96dfa8102705ce47e
acamanes <alain.camanes@free.fr>
2019-01-06 22:41:20 +0100

    add back/forward functions and global variables

--- scripts/drawing.js
+++ scripts/drawing.js
@@ -12,19 +12,21 @@ function e_undefined () {alert("La commande n'existe pas");}
 function e_intialize () {alert("La création des réglettes doit intervenir à la première ligne de code, et uniquement à cette ligne");}
 
 /* Draw the array of reglettes */
-function draw(a, variables, x, y) {
-    var context = variables.get("_context");
+function draw(a, context, x, y) {
+    var pas = 1/a.length;
     for (var i=0; i < a.length; i++) {
+	context.fillStyle="hsl("+Math.round(300*a[i]*pas)+",100%,50%)";
 	context.fillRect(25*i+x, y, 12, -a[i]*10);
+	context.fillStyle="black";
 	context.fillText(i, 25*i+x+4, y+10);
     }
 }
 
 /* Draw the array of reglettes and the instruction */
-function draw_instruction(a, todo, x, y, variables) {
-    var context = variables.get("_context");
-    draw(a, variables, x+100, y),
+function draw_instruction(a, todo, exch, x, y, context) {
+    draw(a, context, x+100, y),
     context.fillText(todo,x,y);
+    context.fillText(exch,x,y-10);
 }
 
 /* Swaps elements indexed i and j of an array */
@@ -195,13 +197,13 @@ function eval(code, variables){
 	    if (todo[0] == "i") {
 		minstructions.get(todo[1])(variables);
 		var reg = variables.get("_reg");
-		drawings.push({"instr":todo[1],"reg":reg.slice(),"exch":variables.get("_lexch")});
+		drawings.push({"instr":todo[1],"line":i,"reg":reg.slice(),"exch":variables.get("_lexch")});
 	    }
 	    /* Functions take arguments and context */
 	    else if (todo[0] == "f") {
 		mfunctions.get(todo[1])(todo[2],variables);
 		    var reg = variables.get("_reg");
-		    drawings.push({"instr":todo[1],"reg":reg.slice()});
+		drawings.push({"instr":todo[1],"line":i,"reg":reg.slice()});
 	    }
 	    /* Loops use previous functions */
 	    else if (todo[0] == "r") {
@@ -209,7 +211,7 @@ function eval(code, variables){
 		    for (var k=0; k<todo[2].length;k++) {
 			eval_simple(todo[2][k], variables);
 			var reg = variables.get("_reg");
-			drawings.push({"instr":todo[2][k][1],"reg":reg.slice(),"exch":variables.get("_lexch")});
+			drawings.push({"instr":todo[2][k][1],"line":i+k,"reg":reg.slice(),"exch":variables.get("_lexch")});
 		    }}}
 	    /* Conditional loops use previous functions */
 	    else if (todo[0] == "w") {
@@ -220,7 +222,7 @@ function eval(code, variables){
 		    for (var k=0; k<todo[1].length;k++) {
 			eval_simple(todo[1][k], variables);
 			var reg = variables.get("_reg");
-			drawings.push({"instr":todo[1][k][1],"reg":reg.slice(),"exch":variables.get("_lexch")});
+			drawings.push({"instr":todo[1][k][1],"line":i+k,"reg":reg.slice(),"exch":variables.get("_lexch")});
 		    }
 		    exch = exch - variables.get("_gexch");
 		}
@@ -233,6 +235,7 @@ function eval(code, variables){
 /* Given the arrays modification history, modifies the canvas and print on it */
 function history(drawings, variables) {
     var context = variables.get("_context");
+    var acontext = variables.get("_contextanimation");
     var reg = variables.get("_reg");
     /* Number of elements */
     var number = (drawings[0].reg).length;
@@ -240,47 +243,55 @@ function history(drawings, variables) {
     var lcode = drawings.length;
     /* Height of 1 drawing instruction */
     var h = 10 * number + 20;
-    /* Modify canvas size */
-    context.canvas.width = number * 30 + 100;
-    context.canvas.height = (h+100) * lcode;
     for (i=0;i<drawings.length;i++){
-	draw_instruction(drawings[i].reg,drawings[i].instr,0,h*(i+1), variables);
-	context.fillText(drawings[i].exch,0,h*(i+1)-10);
+	draw_instruction(drawings[i].reg,drawings[i].instr,drawings[i].exch,0,h*(i+1), context);
     }
     context.fillText("Nombre total d'échanges : "+variables.get("_gexch"),0,10);
 }
 
-function reganimate (drawings, variables){
-    var acanvas = variables.get("_canvasanimation");
-    var acontext = variables.get("_contextanimation");
+function reganimate (drawings, acanvas, acontext){
     var i = 0;
     var max = drawings.length;
     var id = setInterval(frame,1000);
     var number = (drawings[0].reg).length;
-    acontext.canvas.width = number * 30 + 100;
-    acontext.canvas.height = number * 11;
-    // acontext.fillRect(0,10*number,12,-10*(number-1));
-    // acontext.fillRect(12,10*number,12,-10*(number));
+    var pas = 1/number;
     function frame () {
 	if (i==max-1) {clearInterval(id)}
 	else {i++;
 	      acontext.clearRect(0,0,acanvas.width,acanvas.height);
-	      var a = drawings[i].reg;
-	      for (var j=0; j < a.length; j++) {
-		  acontext.fillRect(25*j+100, 10*number, 12, -a[j]*10);
-		  acontext.fillText(j, 25*j+4+100, 10*number+10);
-    }}}
+	      var a = drawings[i];
+	      draw_instruction(a.reg,a.instr,a.exch,0,10*number+50, acontext);
+	     }
+    }
 }
 
 function print (code, variables) {
+    var acanvas = document.getElementById("animation");
+    var acontext = acanvas.getContext("2d");
+    var context = variables.get("_context");
     drawings = eval(code,variables);
+    var number = (drawings[0].reg).length;
+    var h = 10 * number + 20;
+    var lcode = drawings.length;
+    /* Modify canvas size */
+    acontext.canvas.width = number * 30 + 100;
+    acontext.canvas.height = number * 10+60;
+    context.canvas.width = number * 30 + 100;
+    context.canvas.height = (h+100) * lcode;
+    /* Global variables */
+    window.drawings = drawings;
+    window.position = 0;
+    var a = drawings[0];
+    // acontext.clearRect(0,0,acanvas.width,acanvas.height);
+    draw_instruction(a.reg,a.instr,a.exch,0,10*number+50, acontext);
+    // draw(a,acontext,100,10*number);
     history(drawings, variables);
-    reganimate(drawings, variables);
+    var acanvas = document.getElementById("animation");
+    var acontext = acanvas.getContext("2d");
 }
 
-
 /* Button function to execute the code */
-function execute(){
+function execute (){
     var canvas = document.getElementById("reglettes");
     var context = canvas.getContext("2d");
     var animation = document.getElementById("animation");
@@ -299,4 +310,36 @@ function execute(){
     var code = document.getElementById("code").value;
     /* Evaluates */
     print(code, variables);
+}
+
+function back () {
+    var acanvas = document.getElementById("animation");
+    var acontext = acanvas.getContext("2d");
+    var number = (drawings[0].reg).length;
+    if (position == 0) {alert("Début atteint")}
+    else
+    {var a = drawings[position-1];
+     acontext.clearRect(0,0,acanvas.width,acanvas.height);
+    draw_instruction(a.reg,a.instr,a.exch,0,10*number+50, acontext);
+     window.position = window.position-1;}
+}
+
+function fwd () {
+    var acanvas = document.getElementById("animation");
+    var acontext = acanvas.getContext("2d");
+    var number = (drawings[0].reg).length;
+    if (position == drawings.length-1) {alert("Fin atteinte")}
+    else
+    {var a = drawings[position+1];
+     acontext.clearRect(0,0,acanvas.width,acanvas.height);
+     draw_instruction(a.reg,a.instr,a.exch,0,10*number+50, acontext);
+     window.position = window.position+1;}
+}
+
+
+function animation () {
+    var acanvas = document.getElementById("animation");
+    var acontext = acanvas.getContext("2d");
+    reganimate(drawings, acanvas, acontext);
+    window.position = drawings.length-1;
 }
